08f7111cb810a1a42c2945dd75732adf522121d8
temuujindt <temuujindt@gmail.com>
2014-04-29 19:18:31 +0400

    Added main files
    ASP.net files and Docs

---
+++ source-asp/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(source_asp.Startup))]
+namespace source_asp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
